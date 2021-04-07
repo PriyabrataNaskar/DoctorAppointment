74410a30f030e60637ae61bf71515e0a292a4d21
priyabrata <naskar.priyabrata66@gmail.com>
2021-04-08 01:23:52 +0530

    news started

---
+++ app/src/main/java/com/blogspot/priyabratanaskar/firebaselogin/NewsActivity.java
@@ -0,0 +1,14 @@
+package com.blogspot.priyabratanaskar.firebaselogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
